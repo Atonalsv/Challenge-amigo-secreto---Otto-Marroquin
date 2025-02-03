efd97740ed9dd9011ed8932c45e05a8b325f6f66
Atonalsv <otto_alejandro_marroquin@yahoo.com>
2025-02-02 22:37:30 -0600

    Acturalizacion: creacion de funcion para agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creacion array
+let amigos = []
+
+//funcion que agrega amigos al array > muestra alerta > remueve valor del input
+function agregarAmigo() {
+    let input = document.getElementById('amigo').value;
+    if (input === '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(input);
+    }
+    document.getElementById('amigo').value = '';
+    actualizarLista();
+    console.log(amigos);
+    return
+}
+
